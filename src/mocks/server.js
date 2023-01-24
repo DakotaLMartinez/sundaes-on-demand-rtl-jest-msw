d0e3a55544ba8f9b01a132633b2b999c26b40611
DakotaLMartinez <dakotaleemusic@gmail.com>
2023-01-24 11:02:08 -0800

    add test file notes

--- src/mocks/server.js
+++ src/mocks/server.js
@@ -1,5 +1,8 @@
 // src/mocks/server.js
-import { setupServer } from 'msw/node'
-import { handlers } from './handlers'
+import { setupServer } from "msw/node";
+import { handlers } from "./handlers";
+import { setupWorker } from "msw";
 // This configures a request mocking server with the given request handlers.
-export const server = setupServer(...handlers)
+export const server = setupServer(...handlers);
+
+export const worker = setupWorker(...handlers);
