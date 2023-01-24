7341c631ed999c5339be264fb15d00944c320b87
DakotaLMartinez <dakotaleemusic@gmail.com>
2023-01-23 18:54:18 -0800

    add test for disabling order button if no scoops selected

--- src/pages/entry/test/OrderEntry.test.jsx
+++ src/pages/entry/test/OrderEntry.test.jsx
@@ -3,10 +3,36 @@ import {
   screen,
   waitFor,
 } from "../../../test-utils/testing-library-utils";
+import userEvent from "@testing-library/user-event";
 import OrderEntry from "../OrderEntry";
 
 import { rest } from "msw";
 import { server } from "../../../mocks/server";
+
+test("order button is disabled if no scoops are selected", async () => {
+  const user = userEvent.setup();
+  const { unmount } = render(<OrderEntry setOrderPhase={jest.fn()}/>);
+
+  const orderButton = screen.getByRole("button", {
+    name: /order sundae/i,
+  });
+  expect(orderButton).toBeDisabled();
+
+  const vanillaInput = await screen.findByRole("spinbutton", {
+    name: "Vanilla",
+  });
+  await user.clear(vanillaInput);
+  await user.type(vanillaInput, "1");
+
+  expect(orderButton).toBeEnabled();
+
+  await user.clear(vanillaInput);
+  await user.type(vanillaInput, "0");
+  
+  expect(orderButton).toBeDisabled();
+
+  unmount();
+});
 
 test("displays error for scoops and toppings", async () => {
   server.resetHandlers(
